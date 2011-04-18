e58d9ad435081900fcfacee801833e2badb6b8f1
Rinke Hoekstra <hoekstra@uva.nl>
2011-04-18 16:03:52 +0200

    Set default to log to file

--- src/convertall.sh
+++ src/convertall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-python convert_bwb.py --all --no-inline-metadata --no-full-graph --rdf-upload-url http://localhost:3020/servlets/uploadData -user admin -pass visstick --skip-if-existing
+
+python convert_bwb.py --all --no-inline-metadata --no-full-graph --rdf-upload-url http://localhost:3020/servlets/uploadData -user admin -pass visstick --skip-if-existing --log-to-file
 
