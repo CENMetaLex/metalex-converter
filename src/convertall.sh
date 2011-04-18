5522e6d76e63d97f2d440aec121c3494e8a8d48b
Rinke Hoekstra <hoekstra@uva.nl>
2011-04-18 15:50:46 +0200

    Added convertall.sh script

---
+++ src/convertall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python convert_bwb.py --all --no-inline-metadata --no-full-graph --rdf-upload-url http://localhost:3020/servlets/uploadData -user admin -pass visstick --skip-if-existing
+
