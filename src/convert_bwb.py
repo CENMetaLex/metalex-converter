ac588f9d33ad9e4ede24a268e06aef897e2faa8a
Rinke Hoekstra <rjhoekstr@gmail.com>
2011-08-25 16:21:17 +0200

    Rollback

--- src/convert_bwb.py
+++ src/convert_bwb.py
@@ -81,8 +81,7 @@ def getVersionInfo(bwbid):
                     abbreviation = None
                 continue
             
-            rowtext = str(row).replace('\n',' ')
-            match = re.search(r'<tr.*?><td.*?>(<a.*?>)?<p>(<b>)?(\d\d)-(\d\d)-(\d\d\d\d)(</b>)?(</a>)?</p></td><td.*?>(.*?)</td><td.*?>(.*?)</td>', rowtext)
+            match = re.search(r'<tr.*?><td.*?><p><b>(\d\d)-(\d\d)-(\d\d\d\d)</b></p></td><td.*?>(.*?)</td><td.*?>(.*?)</td>', str(row))
             if match :
 #                logging.debug("1 pre {0}", match.group(5))
                 if match.group(5) != "" and match.group(5) != "<p></p>":
