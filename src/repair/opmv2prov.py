9b967f662bff09d7bd7377be11e2c8f6c3de784d
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-02-06 16:45:43 +0100

    Fixed, working, tested, done

--- src/repair/opmv2prov.py
+++ src/repair/opmv2prov.py
@@ -39,20 +39,20 @@ class FunctieHandler(xml.sax.ContentHandler):
         
     def characters(self, content):
         if self.functie == True:
-            agent_title = content.strip(' ,.:;').encode('utf-8')
+            agent_title = content.replace('\n',' ').replace('\t',' ').strip(' ,.:;').encode('utf-8')
+            if agent_title != '':
+                 agent_uri = 'http://doc.metalex.eu/id/agent/' + urllib2.quote(agent_title.replace(' ','_'))
             
-            agent_uri = 'http://doc.metalex.eu/id/agent/' + urllib2.quote(agent_title.replace(' ','_'))
+                 self.write(self.process_uri,TYPE,ACTIVITY)
+                 self.write(self.expression_uri,TYPE,ENTITY)
             
-            self.write(self.process_uri,TYPE,ACTIVITY)
-            self.write(self.expression_uri,TYPE,ENTITY)
-            
-            self.writed(self.process_uri,ENDED,self.date)
-            self.write(self.expression_uri,GENERATED,self.process_uri)
-            self.writed(self.expression_uri,GENERATEDTIME,self.date)
-            self.write(self.process_uri,ASSOCIATED,agent_uri)
-            self.write(self.expression_uri,ATTRIBUTED,agent_uri)
-            self.writes(agent_uri,NAME,agent_title)
-            self.write(agent_uri,TYPE,AGENT)
+                 self.writed(self.process_uri,ENDED,self.date)
+                 self.write(self.expression_uri,GENERATED,self.process_uri)
+                 self.writed(self.expression_uri,GENERATEDTIME,self.date)
+                 self.write(self.process_uri,ASSOCIATED,agent_uri)
+                 self.write(self.expression_uri,ATTRIBUTED,agent_uri)
+                 self.writes(agent_uri,NAME,agent_title)
+                 self.write(agent_uri,TYPE,AGENT)
             
             
 
@@ -61,10 +61,10 @@ class FunctieHandler(xml.sax.ContentHandler):
         out.write("<{}> <{}> <{}> .\n".format(s,p,o))
         
     def writes(self,s,p,string):
-        out.write("<{}> <{}> \"{}\"^^http://www.w3.org/2001/XMLSchema#string .\n".format(s,p,string))
+        out.write("<{}> <{}> \"{}\"^^<http://www.w3.org/2001/XMLSchema#string> .\n".format(s,p,string))
         
     def writed(self,s,p,date):
-        out.write("<{}> <{}> \"{}\"^^http://www.w3.org/2001/XMLSchema#date .\n".format(s,p,date))
+        out.write("<{}> <{}> \"{}\"^^<http://www.w3.org/2001/XMLSchema#date> .\n".format(s,p,date))
         
 
 
@@ -75,7 +75,6 @@ if __name__ == '__main__':
     path = sys.argv[1]
     outpath = sys.argv[2]
     out = open(outpath,'w')
-    print len(glob.glob(path))
     for mlfile in glob.glob(path):
         try :
             xml.sax.parse(open(mlfile),FunctieHandler())
