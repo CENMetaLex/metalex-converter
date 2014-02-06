7e48ce7b373c4f1369d36c251a40b4ae901d3b11
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-02-06 16:46:23 +0100

    Generating parent URIs from MetaLex source files (does not work for opaque uris)

--- src/repair/parent.py
+++ src/repair/parent.py
@@ -1,58 +1,32 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+import xml.sax
+import urllib2
+import glob
 import re
+import sys
 
-hcontainers = ["afdeling","bijlage","boek","circulaire.divisie","citaat-artikel","deel","divisie","hoofdstuk","model","officiele-inhoudsopgave","paragraaf","sub-paragraaf","titeldeel","verdragtekst","wetcitaat","wijzig-artikel","wijzig-divisie","wijzig-lid-groep","artikel"]
-
-ENDPOINT = 'http://doc.metalex.eu:8000/sparql/'
 PARENT = 'http://www.metalex.eu/schema/1.0#parent'
 
-outfile = 'mappings_{}.nt'
-posfile = 'last'
-
-
-
-if __name__ == '__main__':
-    print "This script queries the MDS for all expressions, and generates 'parent' relations between expressions and their parent HContainer elements"
+class ExpressionHandler(xml.sax.ContentHandler):
     
-    sparql = SPARQLWrapper(ENDPOINT)
-    sparql.setReturnFormat(JSON)
-    
-    query = """
-        PREFIX metalex: <http://www.metalex.eu/schema/1.0#>
-        SELECT DISTINCT ?e WHERE {{
-            ?e a metalex:BibliographicExpression .
-        }} LIMIT {0} OFFSET {1}
-    """
-    
-    step = 200000
-    pos  = int(open(posfile,'r').read())
-    
-    while True:
-        print "OFFSET {} LIMIT {}".format(pos,step)
-        out = open(outfile.format(pos),'w')
+    def __init__(self):
+        xml.sax.ContentHandler.__init__(self)
         
-        sparql.setQuery(query.format(step,pos))
-        print "Running query",
-        results = sparql.query().convert()
-        print "... done"
-        if len(results) < 1 :
-            print "No more results"
-            out.close()
-            break
-            
-        for r in results['results']['bindings'] :
-            expression = r['e']['value']
+    def startElement(self, name, attrs):
+        if 'about' in attrs:
+            expression = attrs.getValue("about")
+
             m = re.search('(?P<regulation>.*BWB.*?)/(?P<path>.*/)?(?P<hcontainer>(artikel|enig-artikel|aanhef|artikel\.toelichting|preambule|deel|bijlage|definitie|noot|circulaire.divisie|circulaire|regeling-sluiting|verdragtekst|titeldeel|hoofdstuk|paragraaf|regeling|wijzig-artikel|afdeling)/.+?/)(.*/)?(?P<lang>\w{2})?/(?P<date>\d\d\d\d-\d\d-\d\d)', expression)
 
             if not m:
                 m = re.search('(?P<regulation>.*BWB.*?)/(.*/)?(?P<lang>\w{2})?/(?P<date>\d\d\d\d-\d\d-\d\d)', expression)
-            
+        
             if not m:
                 m = re.search('(?P<regulation>.*BWB.*?)/(.*/)?(?P<date>\d\d\d\d-\d\d-\d\d)', expression)
-            
+        
             if not m :
-                print m
-                continue
+                print expression
+                return
+                
             parent = ""
             groups = m.groupdict()
             for p in ['regulation','path','hcontainer','lang','date'] :
@@ -62,14 +36,39 @@ if __name__ == '__main__':
             parent = parent.rstrip('/')
             parent = parent.replace('//','/')
             parent = parent.replace('http:/doc','http://doc')
-               
-            out.write("<{}> <{}> <{}> . \n".format(expression,PARENT,parent))
-        
-        pos += step
-        
-        open(posfile,'w').write(str(pos))
-        out.close()
+           
+            if not parent == expression :
+                out.write("<{}> <{}> <{}> . \n".format(expression,PARENT,parent))
+                
             
+    def write(self,s,p,o):
+        out.write("<{}> <{}> <{}> .\n".format(s,p,o))
+        
+    def writes(self,s,p,string):
+        out.write("<{}> <{}> \"{}\"^^<http://www.w3.org/2001/XMLSchema#string> .\n".format(s,p,string))
+        
+    def writed(self,s,p,date):
+        out.write("<{}> <{}> \"{}\"^^<http://www.w3.org/2001/XMLSchema#date> .\n".format(s,p,date))
+        
+
+
+if __name__ == '__main__':
+    print "Usage: parent [MetaLexFilesMask] [OutFile.nt]"
+    
+    print sys.argv
+    path = sys.argv[1]
+    outpath = sys.argv[2]
+    out = open(outpath,'w')
+    for mlfile in glob.glob(path):
+        try :
+            xml.sax.parse(open(mlfile),ExpressionHandler())
+        except Exception as e :
+            print mlfile
+            print e
+
+    out.close()
+
+
 
 
     
