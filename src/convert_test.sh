2ae7067d786f77fb37a2611778ddf1ab440fcfc3
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-02-10 11:03:40 +0100

    Added simple test script

---
+++ src/convert_test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python convert_bwb.py --data-dir /Users/hoekstra/projects/metalex-converter/test/ --out-dir /Users/hoekstra/projects/metalex-converter/test/ --no-report --bwbid BWBR0008467 --no-graph --no-full-graph
+
