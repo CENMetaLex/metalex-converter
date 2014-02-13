2928e00edca8c1fd11ee6ddf5fa7fafb20b56570
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-02-13 16:25:26 +0100

    Split parent and short, now generating short URIs with collision handling.

--- src/repair/short_repair.py
+++ src/repair/short_repair.py
@@ -9,9 +9,7 @@ import sys
 hcontainers = ['bijlage', 'wijzig-divisie', 'circulaire', 'afdeling', 'deel', 'definitie', 'boek', 'wijzig-artikel', 'aanhef', 'noot', 'wetcitaat', 'regeling', 'hoofdstuk', 'preambule', 'sub-paragraaf', 'titeldeel', 'wijzig-lid-groep', 'divisie', 'artikel.toelichting', 'artikel', 'citaat-artikel', 'officiele-inhoudsopgave', 'model', 'artikel.toelichtingartikel', 'paragraaf', 'verdragtekst', 'circulaire.divisie', 'enig-artikel', 'regeling-sluiting']
 # shortlist = ['bijlage', 'circulaire', 'afdeling', 'deel', 'definitie', 'boek', 'wijzig-artikel', 'aanhef', 'noot', 'wetcitaat', 'regeling', 'hoofdstuk', 'preambule', 'sub-paragraaf', 'titeldeel', 'wijzig-lid-groep', 'artikel.toelichting', 'artikel', 'citaat-artikel', 'officiele-inhoudsopgave', 'model', 'artikel.toelichtingartikel', 'paragraaf', 'verdragtekst', 'enig-artikel', 'regeling-sluiting']
 shortlist = ['artikel']
-antishortlist = ['bijlage']
 
-PARENT = 'http://www.metalex.eu/schema/1.0#parent'
 REALIZES = 'http://www.metalex.eu/schema/1.0#realizes'
 SAMEAS = 'http://www.w3.org/2002/07/owl#sameAs'
 
@@ -19,17 +17,15 @@ GENERATEDBY = 'http://www.w3.org/ns/prov#wasGeneratedBy'
 GENERATEDAT = 'http://www.w3.org/ns/prov#wasGeneratedAt'
 
 
+shorts = {}
+
 class ExpressionHandler(xml.sax.ContentHandler):
     
     parents = []
-    parent_types = []
     def __init__(self):
         xml.sax.ContentHandler.__init__(self)
         
     def startElement(self, name, attrs):
-        # print self.parents
-        #print attrs
-        # print self.attributes
         if name == 'meta':
             return
         
@@ -39,17 +35,16 @@ class ExpressionHandler(xml.sax.ContentHandler):
         
         # print name, expression, c, self.parents
         if expression :      
-            if self.parents != [] :
-                out.write("<{}> <{}> <{}> . \n".format(expression,PARENT,self.parents[-1]))
                 
             m = re.search('(/(?P<lang>\w\w)/)?(?P<date>\d\d\d\d-\d\d-\d\d)',expression)
             if m:
                 d = m.group('date')
+            else :
+                d = None
                 
             
             if str(c) in hcontainers or name == 'root':
                 self.parents.append(expression)
-                
                 
                 m = re.search('(?P<bwb>.*/BWB.*?/)(?P<path>.*/)?(?P<hcontainer>{}/.*?)(?P<version>/.*)'.format(c),expression)
                 
@@ -65,27 +60,25 @@ class ExpressionHandler(xml.sax.ContentHandler):
                     
                     out.write("<{}> <{}> <{}> . \n".format(expression,GENERATEDBY,activity))
                     
+                    
+                    
+                    
+                    
+                    
                     if d:
                         out.write("<{}> <{}> \"{}\"^^<http://www.w3.org/2001/XMLSchema#date> . \n".format(expression,GENERATEDAT,d))
                     
-                    if self.parent_types != []:
-                        if expression != short and str(c) in shortlist and not self.parent_types[-1] in antishortlist :
-                            # print str(c), self.parent_types[-1], expression
-                            out.write("<{}> <{}> <{}> . \n".format(expression,SAMEAS,short))
-                        if work != shortwork and str(c) in shortlist and not self.parent_types[-1] in antishortlist:
-                            # print str(c), self.parent_types[-1], expression
-                            out.write("<{}> <{}> <{}> . \n".format(work,SAMEAS,shortwork))
-                            out.write("<{}> <{}> <{}> . \n".format(expression,REALIZES,shortwork))
+
+                    if expression != short and str(c) in shortlist :
+                        shorts.setdefault(short,[]).append({'e': expression, 'w': work, 'shortw': shortwork})
                 
-                self.parent_types.append(str(c))
+
                 
             elif self.parents != [] :
                 self.parents.append(self.parents[-1])
-                self.parent_types.append(self.parent_types[-1])
                 
         else :
             self.parents.append(self.parents[-1])
-            self.parent_types.append(self.parent_types[-1])
             
 
 
@@ -95,8 +88,6 @@ class ExpressionHandler(xml.sax.ContentHandler):
     def endElement(self, name):
         if self.parents != [] :
             del self.parents[-1]
-        if self.parent_types != []:
-            del self.parent_types[-1]
        
                 
 
@@ -113,6 +104,19 @@ if __name__ == '__main__':
         print mlfile
         try :
             xml.sax.parse(open(mlfile),ExpressionHandler())
+            
+            for short,v in shorts.items():
+                if len(v) > 1 :
+                    print "Collission detected for {}".format(short)
+                    continue
+                else :
+                    expression = v[0]['e']
+                    work = v[0]['w']
+                    shortwork = v[0]['shortw']
+                    
+                    out.write("<{}> <{}> <{}> . \n".format(expression,SAMEAS,short))
+                    out.write("<{}> <{}> <{}> . \n".format(work,SAMEAS,shortwork))
+                    out.write("<{}> <{}> <{}> . \n".format(expression,REALIZES,shortwork))
         except Exception as e :
             print mlfile
             print e
